4cf6922c451461845c7ddfbfa1cee255bbe856a9
nbahamonn <n.bahamonn@uniandes.edu.co>
2019-10-31 16:25:36 +0000

    Se calcula el perimetro, diametro y area del radio del radio ingresado

--- 1.cpp
+++ 1.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
+#include <cmath>
 
 int main(void){
     
     int r;
+    int d,
+    int p;
+    int a;
 
     std::cout <<"Radio del Circulo\n";
     std::cin >> r;
+    d = r*r
+    p = 2*M_PI*r
+    a = M_PI*r*r
       
 return 0;
 }

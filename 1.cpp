6d1e972162ad769569de718fd0558e74f925176f
nbahamonn <n.bahamonn@uniandes.edu.co>
2019-10-31 16:31:25 +0000

    Se Calcula y muestra los valores del Diametro, Perimetro y Area

--- 1.cpp
+++ 1.cpp
@@ -4,15 +4,19 @@
 int main(void){
     
     int r;
-    int d,
-    int p;
-    int a;
+    int d;
+    float p;
+    float a;
 
     std::cout <<"Radio del Circulo\n";
     std::cin >> r;
-    d = r*r
-    p = 2*M_PI*r
-    a = M_PI*r*r
+    d = r*2;
+    p = 2*M_PI*r;
+    a = M_PI*r*r;
+    
+    std::cout <<"Diametro = " << d << "\n";
+    std::cout <<"Perimetro = " << p << "\n";
+    std::cout <<"Area = " << a << "\n";
       
 return 0;
 }

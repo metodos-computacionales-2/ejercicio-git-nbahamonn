1c55d0ae23ca1437dc5d64b2b054881c31640458
nbahamonn <n.bahamonn@uniandes.edu.co>
2019-10-31 16:37:29 +0000

    Se ingresan los tres numeros enteros

--- 2.cpp
+++ 2.cpp
@@ -6,6 +6,16 @@ int main(void){
     int a;
     int b;
     int c;
-     
+    
+    std::cout <<"Primer Numero\n";
+    std::cin >> a;
+    std::cout <<"Segundo Numero\n";
+    std::cin >> b;
+    std::cout <<"Tercer Numero\n";
+    std::cin >> c;
+    
+    
+    
+    
 return 0;
 }
